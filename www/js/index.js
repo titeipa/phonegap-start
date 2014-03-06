2b79e716399460ced024545d318da278586bc248
Țiței Paul Adrian <titei.pa@gmail.com>
2014-03-06 13:58:05 +0200

    Buttons

--- www/js/index.js
+++ www/js/index.js
@@ -19,27 +19,12 @@
 
 var app = {
     initialize: function () {
-        this.bind();
+        this.bind(); 
     },
     bind: function () {
         document.addEventListener('deviceready', this.deviceready, false);
     },
     deviceready: function () {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://api.github.com/legacy/repos/search/javascript', true);
-        // Response handlers.
-        xhr.onload = function () {
-            var repos = JSON.parse(xhr.response), i, reposHTML = "";
-            for (i = 0; i < repos.repositories.length; i++) {
-                reposHTML += "<p><a href='https://github.com/" + repos.repositories[i].username + "/" + repos.repositories[i].name + "'>" + repos.repositories[i].name + "</a><br>" + repos.repositories[i].description + "</p>";
-            }
-            document.getElementById("allRepos").innerHTML = reposHTML;
-        };
-
-        xhr.onerror = function () {
-            alert('error making the request.');
-        };
-
-        xhr.send();
+        
     }
 };
