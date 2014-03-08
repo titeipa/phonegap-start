f2639ca0fc1743bff623037fed9f4fe185205da0
Țiței Paul Adrian <titei.pa@gmail.com>
2014-03-08 15:28:27 +0200

    Moved code so that it runs when device is ready.

--- www/js/index.js
+++ www/js/index.js
@@ -25,15 +25,14 @@ var app = {
         document.addEventListener('deviceready', this.deviceready, false);
     },
     deviceready: function () {
-        
+        $(function() {
+            $.each(['#f00', '#ff0', '#0f0', '#0ff', '#00f', '#f0f', '#000', '#fff'], function() {
+                $('#container .tools').append("<a href='#sketch' data-color='" + this + "' style='width: 10px; background: " + this + ";'></a> ");
+            });
+            $.each([3, 5, 10, 15], function() {
+                $('#container .tools').append("<a href='#sketch' data-size='" + this + "' style='background: #ccc'>" + this + "</a> ");
+            });
+            $('#sketch').sketch();
+        });   
     }
 };
-$(function() {
-    $.each(['#f00', '#ff0', '#0f0', '#0ff', '#00f', '#f0f', '#000', '#fff'], function() {
-        $('#container .tools').append("<a href='#sketch' data-color='" + this + "' style='width: 10px; background: " + this + ";'></a> ");
-    });
-    $.each([3, 5, 10, 15], function() {
-        $('#container .tools').append("<a href='#sketch' data-size='" + this + "' style='background: #ccc'>" + this + "</a> ");
-    });
-    $('#sketch').sketch();
-});
